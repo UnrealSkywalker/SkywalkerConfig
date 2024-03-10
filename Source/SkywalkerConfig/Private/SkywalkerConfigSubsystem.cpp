00532b6a2e62fa8125585bc818b95219641d8306
Shyfan <410713847@qq.com>
2024-03-10 23:31:25 +0800

    保存

--- Source/SkywalkerConfig/Private/SkywalkerConfigSubsystem.cpp
+++ Source/SkywalkerConfig/Private/SkywalkerConfigSubsystem.cpp
@@ -1,4 +1,59 @@
 // Fill out your copyright notice in the Description page of Project Settings.
 
-
 #include "SkywalkerConfigSubsystem.h"
+
+#include "Engine/AssetManager.h"
+
+DEFINE_LOG_CATEGORY_STATIC(SkywalkerConfig, Log, All);
+
+bool USkywalkerConfigSubsystem::LoadDataTableAsset(const TArray<FAssetData>& ArrayAssetData)
+{
+	LoadFinish = false;
+
+	TArray<FSoftObjectPath> TArrayTableDataPath;
+	for (int32 i = 0; i < ArrayAssetData.Num(); ++i)
+	{
+		TArrayTableDataPath.AddUnique(ArrayAssetData[i].ToSoftObjectPath());
+	}
+
+	//ResourceManager->LoadAsync(TArrayTableDataPath);
+
+	FStreamableManager& Streamable = UAssetManager::Get().GetStreamableManager();
+	StreamableHandle = Streamable.RequestAsyncLoad(TArrayTableDataPath, FStreamableDelegate::CreateUObject(this, &USkywalkerConfigSubsystem::DataTableAssetLoadComplete));
+
+	return true;
+}
+
+bool USkywalkerConfigSubsystem::IsLoadFinish()
+{
+	return LoadFinish;
+}
+
+void USkywalkerConfigSubsystem::DataTableAssetLoadComplete()
+{
+	TArray<UObject*> OutObjects;
+	StreamableHandle->GetLoadedAssets(OutObjects);
+	for (int32 i = 0; i < OutObjects.Num(); ++i)
+	{
+		UE_LOG(SkywalkerConfig, Warning, TEXT("Name::::::::%s"), *OutObjects[i]->GetName());
+
+		TSortedMapNameToUDataTable.Add(OutObjects[i]->GetName(), Cast<UDataTable>(OutObjects[i]));
+	}
+
+	LoadFinish = true;
+}
+
+UDataTable* USkywalkerConfigSubsystem::GetDataTable(FString inName)
+{
+	//UE_LOG(ETDConfigSubsystem, Log, TEXT("GetDataTable \"%s\""), *inName);
+
+	UDataTable* DataTable = *TSortedMapNameToUDataTable.Find(inName);
+	if (nullptr != DataTable)
+	{
+		return DataTable;
+	}
+
+	UE_LOG(SkywalkerConfig, Error, TEXT("GetDataTable \"%s\" FAILED"), *inName);
+
+	return nullptr;
+}
--- Source/SkywalkerConfig/Public/SkywalkerConfigSubsystem.h
+++ Source/SkywalkerConfig/Public/SkywalkerConfigSubsystem.h
@@ -4,13 +4,37 @@
 
 #include "CoreMinimal.h"
 #include "Subsystems/GameInstanceSubsystem.h"
+#include "Containers/SortedMap.h"
+#include "Engine/Datatable.h"
+#include "Engine/StreamableManager.h"
+
 #include "SkywalkerConfigSubsystem.generated.h"
 
 /**
- * 
+ *
  */
 UCLASS()
 class SKYWALKERCONFIG_API USkywalkerConfigSubsystem : public UGameInstanceSubsystem
 {
 	GENERATED_BODY()
+
+public:
+
+	UDataTable* GetDataTable(FString inName);
+
+	UFUNCTION(BlueprintCallable, Category = "ETDConfig")
+	bool LoadDataTableAsset(const TArray<FAssetData>& ArrayAssetData);
+
+	UFUNCTION(BlueprintCallable, Category = "ETDConfig")
+	bool IsLoadFinish();
+
+protected:
+	void DataTableAssetLoadComplete();
+
+private:
+	TSortedMap<FString, UDataTable*> TSortedMapNameToUDataTable;
+
+	bool LoadFinish;
+
+	TSharedPtr<FStreamableHandle> StreamableHandle;
 };
